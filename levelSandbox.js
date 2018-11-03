b1e328117bfa73a07ef6efe636ec33e586ee20df
Justin Solms <justinsolms@gmail.com>
2018-11-03 11:07:28 +0200

    Refactor levelSandbox.getLevelDBData

--- levelSandbox.js
+++ levelSandbox.js
@@ -19,18 +19,21 @@ class LevelSandbox {
     let self = this;
 
     return new Promise(function(resolve, reject) {
-      self.db.get(key, (err, value) => {
-        if (err) {
-          if (err.type == 'NotFoundError') {
-            resolve(undefined);
-          } else {
-            console.log('Block ' + key + ' get failed', err);
-            reject(err);
-          }
-        } else {
-          resolve(value);
-        }
-      });
+      // self.db.get(key, (err, value) => {
+      //   if (err) {
+      //     if (err.type == 'NotFoundError') {
+      //       resolve(undefined);
+      //     } else {
+      //       console.log('Block ' + key + ' get failed', err);
+      //       reject(err);
+      //     }
+      //   } else {
+      //     resolve(value);
+      //   }
+      // });
+      self.db.get(key)
+        .then((value) => {resolve(value)})
+        .catch((err) => {reject('Block ' + key + ' get failed : ' + err)})
     });
   }
 
@@ -41,18 +44,9 @@ class LevelSandbox {
     let self = this;
 
     return new Promise(function(resolve, reject) {
-      // self.db.put(key, value, function(err) {
-      //   if (err) {
-      //     console.log('Block ' + key + ' submission failed', err);
-      //     reject(err);
-      //   }
-      //   resolve(value);
-      // });
       self.db.put(key, value)
         .then(() => {resolve(value)})
-        .catch((err) => {
-          reject('Block ' + key + ' submission failed : ' + err);
-        })
+        .catch((err) => { reject('Block ' + key + ' submission failed : ' + err); })
     });
   }
 
@@ -62,19 +56,6 @@ class LevelSandbox {
     let self = this;
 
     return new Promise(function(resolve, reject) {
-      // let i = 0;
-      // self.db.createReadStream()
-      //   .on('data', function(data) {
-      //     i++;
-      //   })
-      //   .on('error', function(err) {
-      //     reject('Unable to read data stream!' + err);
-      //   })
-      //   .on('close', function() {
-      //     self.addLevelDBData(i, value);
-      //     // Resolve with the added value
-      //     resolve(i)
-      //   });
       self.getBlocksCount()
         .then((count) => {
           self.addLevelDBData(count, value)
